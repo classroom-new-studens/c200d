8689d5ae9baff3e27b4dd770def8d6a55fbdbc0f
Games Unblocked <Brainmassage@outlook.com>
2024-08-17 23:34:19 +0300

    awe

--- assets/js/chunk-2d0b9093.1723873339468.js
+++ assets/js/chunk-2d0b9093.1723873339468.js
@@ -1,14 +1,1 @@
-(window['wMmNnWW'] = window['wMmNnWW'] || [])['push']([
-    ['chunk-2d0b9093'],
-    {
-        '30dd': function (a, b, d) {
-            'use strict';
-            d['r'](b);
-            var f = function () {
-                    var k = this, m = k['$createElement'], p = k['WwNMnWmw']['_c'] || m;
-                    return p('router-view');
-                }, g = [], h = d('2877'), i = {}, j = Object(h['a'])(i, f, g, !0x1, null, null, null);
-            b['default'] = j['exports'];
-        }
-    }
-]);
+No Content: https://kirka.io/assets/js/chunk-2d0b9093.1723873339468.js
